d06893af4e288f4eb8e0adb50ce4bec77a4cc218
Janvi Patel <jn398689@dal.ca>
2022-02-12 01:50:02 -0400

    Initial phase pipeline building

---
+++ backend/src/test/java/ca/dal/cs/wanderer/DemoApplicationTests.java
@@ -0,0 +1,15 @@
+package ca.dal.cs.wanderer;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+		Assertions.assertEquals(5, 2+3);
+	}
+
+}
