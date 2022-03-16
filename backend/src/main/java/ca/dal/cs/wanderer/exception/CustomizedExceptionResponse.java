f8cd5155401aa0967da6b43dc6a80020e368e7c4
Meghdoot <mg770477@dal.ca>
2022-03-16 15:35:08 -0300

    Global Exception and Configuration for REST APIs

---
+++ backend/src/main/java/ca/dal/cs/wanderer/exception/CustomizedExceptionResponse.java
@@ -0,0 +1,34 @@
+package ca.dal.cs.wanderer.exception;
+
+import ca.dal.cs.wanderer.exception.category.EmailNotFound;
+import ca.dal.cs.wanderer.util.ErrorMessages;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+
+@ControllerAdvice
+public class CustomizedExceptionResponse extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date()
+                , ex.getMessage()
+                , request.getDescription(false));
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(EmailNotFound.class)
+    public final ResponseEntity<Object> emailNotFound(EmailNotFound ex, WebRequest request) {
+ExceptionResponse response=new ExceptionResponse(new Date(),ex.getMessage(), request.getDescription(false));
+GenericResponse<ExceptionResponse> genericResponse=new GenericResponse<>(false, ErrorMessages.EMAIL_NOT_FOUND.getHttpStatus().name(),response);
+return handleExceptionInternal(ex, genericResponse,new HttpHeaders(),ErrorMessages.EMAIL_NOT_FOUND.getHttpStatus(),request);
+    }
+
+
+}
