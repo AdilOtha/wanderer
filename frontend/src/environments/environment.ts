58356322889008e150a3bf69a33b1f307db9493c
Adil <adil@floatbot.ai>
2022-08-12 12:03:06 -0300

    modified Angular env file

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -5,23 +5,6 @@
 export const environment = {
   production: false,
 
-  mapbox: {
-    accessToken:
-      'pk.eyJ1IjoiYWRpbG90aGEiLCJhIjoiY2s2dnY4ZW5wMDFnejNucW9ncnF2c3hwcyJ9.-NzSatK_aFkiLpKXsd6BAA',
-  },
-
-  firebase: {
-    apiKey: 'AIzaSyA5oDQrFKf_sjAAkBJzJFfE7kQcit2Hhgo',
-    authDomain: 'wanderer-341922.firebaseapp.com',
-    projectId: 'wanderer-341922',
-    storageBucket: 'wanderer-341922.appspot.com',
-    messagingSenderId: '381531134830',
-    appId: '1:381531134830:web:9f48183efdc6b669cfff7f',
-    measurementId: 'G-PJNEBC17EC',
-  },
-
-  googleMaps: 'AIzaSyBHkVH8NiHsuH4cU-GsZC38GXLvxoxXSjo',
-
   APIENDPOINT: 'http://localhost:8080/',
 
   OAUTH_REDIRECT_URL: 'http://localhost:8080/oauth2/authorization/google',
